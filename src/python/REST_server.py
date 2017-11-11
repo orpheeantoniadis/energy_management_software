666ae2b69394d01a523bfd7e959b21f194bd1437
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2017-11-11 21:50:59 +0100

    add a function to get pi controllers from db

--- src/python/REST_server.py
+++ src/python/REST_server.py
@@ -6,6 +6,10 @@ from database import *
 
 app = Flask(__name__)
 
+@app.route('/controllers_list', methods=['GET'])
+def get_all_controllers():
+	return jsonify(db.select_all_controllers())
+
 """
 @api {get} /:controller/:sensor/last_measures Sensor Last Measures
 @apiName GetSensorLastMeasures
